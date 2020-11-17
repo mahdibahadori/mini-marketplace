2b69616a94e8a37a4fba232c39b167c1e7dcd7d8
Mahdi Bahadori <m.bahadori1976@gmail.com>
2020-11-17 23:58:26 +1100

    two unused tables deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_081026) do
+ActiveRecord::Schema.define(version: 2020_11_17_125712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_081026) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "conversations", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "receiver_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.string "category"
@@ -53,17 +46,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_081026) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "conversation_id", null: false
-    t.bigint "user_id", null: false
-    t.boolean "read", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,6 +63,4 @@ ActiveRecord::Schema.define(version: 2020_11_10_081026) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
 end
